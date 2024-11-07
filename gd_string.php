eb29f578ae490aa9215e66e0d06fc723b3eea97b
kk-outlaw <kk.outlaw.39@gmail.com>
2024-11-08 00:11:53 +0900

    余計なコメントを削除

--- gd_string.php
+++ gd_string.php
@@ -19,13 +19,6 @@ $min_x = min($bounding_box[0], $bounding_box[2], $bounding_box[4], $bounding_box
 $max_y = max($bounding_box[1], $bounding_box[3], $bounding_box[5], $bounding_box[7]);
 $min_y = min($bounding_box[1], $bounding_box[3], $bounding_box[5], $bounding_box[7]);
 
-/*/
-echo '(' . $bounding_box[0] . ', ' . $bounding_box[1] . ')' . "\n";
-echo '(' . $bounding_box[2] . ', ' . $bounding_box[3] . ')' . "\n";
-echo '(' . $bounding_box[4] . ', ' . $bounding_box[5] . ')' . "\n";
-echo '(' . $bounding_box[6] . ', ' . $bounding_box[7] . ')' . "\n";
-//*/
-
 //画像サイズ
 $canvas_width = $max_x - $min_x;
 $canvas_height = $max_y - $min_y;
@@ -36,7 +29,6 @@ $image = imagecreatetruecolor($canvas_width, $canvas_height);
 $background_color = imagecolorallocate($image, 255, 255, 255); //背景色: FFFFFF
 $foreground_color = imagecolorallocate($image, 0, 0, 255); //前景色：: 0000FF
 
-//*
 //画像を背景色で塗りつぶす
 imagefilledrectangle($image, 0, 0, $canvas_width, $canvas_height, $background_color);
 
@@ -61,16 +53,4 @@ imagepng($image, null, -1, -1);
 
 //後処理
 imagedestroy($image);
-/*/
-
-/*
-例：$text_angle = 100, $font_size = 32, フォントをNotoSerifJP-Regular.ttfとした場合のバウンディングボックスの座標は
-($bounding_box[0], $bounding_box[1]) = (12, -2)
-($bounding_box[2], $bounding_box[3]) = (-66, -449)
-($bounding_box[4], $bounding_box[5]) = (-115, -440)
-($bounding_box[6], $bounding_box[7]) = (-36, 7)
-
-この中でx座標、y座標の最小値(-115, -449) = ($min_x, $min_y)がキャンバスの起点となるように調整する
-すなわちimagettftext()の開始位置は($min_x * -1, $min_y * -1)となる
-*/
 ?>
